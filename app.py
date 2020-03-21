2ce6d6396cd62a458b2ad79650b964bf46b0950a
mibkvr <40341638+mibkvr@users.noreply.github.com>
2020-03-21 10:56:45 +0100

    Created Blueprints and 404 / 405

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from report.ticket import main_page
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+app.register_blueprint(main_page)
+
+@app.errorhandler(404)
+def handle_404_error(e):
+    return {'error':'Page not found'},404
+
+@app.errorhandler(405)
+def handle_405_error(e):
+    return jsonify({'error':'Method not allowed'}),404
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
