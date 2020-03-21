19a4478591693d0d71f5d9b71b07ba9d503f4515
Cedi99 <43995750+Cedi99@users.noreply.github.com>
2020-03-21 13:01:07 +0100

    Datenbank

--- report/__init__.py
+++ report/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
 
 
 from report.Blueprints.routes import main_page
---
+++ report/database.py
@@ -0,0 +1,28 @@
+from report import db
+from report import ma
+
+
+class Ticket(db.Model):
+    ticketId = db.Column(db.Integer, primary_key=True)
+    taskId = db.Column(db.Integer)
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+    TicketType = db.Column(db.String)
+
+    def __init__(self, taskId, title, body, TicketType):
+        self.taskId = taskId
+        self.title = title
+        self.body = body
+        self.TicketType = TicketType
+
+
+class TicketSchema(ma.Schema):
+    class Meta:
+        fields = ('ticketId', 'taskId', 'title', 'body', 'TicketType')
+
+
+ticketSchema = TicketSchema()
+ticketSchema_many = TicketSchema(many=True)
+
+db.create_all()
+
