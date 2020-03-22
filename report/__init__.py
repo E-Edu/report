16562ab0d35a9a70b5e5faedc743466086e15335
mibkvr <40341638+mibkvr@users.noreply.github.com>
2020-03-22 12:40:56 +0100

    Try to add Gunicon

--- report/__init__.py
+++ report/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
 try:
     db_path = "mysql+pymysql://" + os.environ.get("DATABASE_USERNAME") + ":" + os.environ.get(
         "DATABASE_PASSWORD") + "@" + os.environ.get("DATABASE_HOSTNAME") + ":" + os.environ.get(
@@ -21,6 +22,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+
+db.create_all()
 
 from report.Blueprints.routes import main_page
 from report.Blueprints.error_handler import app_error
